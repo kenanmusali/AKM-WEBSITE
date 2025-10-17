8282a6393d3b2d57f4c3baf7d1e92685b17e80d3
kenanmusali <textme0306@gmail.com>
2025-10-17 16:30:33 +0400

    Updated

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -10,23 +10,23 @@ const Footer = () => {
         <div className='Footer-Group'>
             <div className="Footer Center-Objects ">
                 <img src={logoIMG} class="Logo-Marks No-Select" />
-                <div className="Main-Button">
-                    <a className='Link-Fotter' href="https://www.linkedin.com/company/103794108/" target="_blank">
-                        <p className='Social-Text'>BİZ SOSİAL ŞƏBƏKƏDƏN İZLƏYİN</p>
-                        {/* <div className="social-icons">
-                            <img src={inIMG} class=" No-Select" />
-                            <a href="https://www.facebook.com/absheroncareer/" target="_blank">
+                <div className="social-footer-group">
+                    <p>Biz sosial şəbəkələrdən izləyin:</p>
+                    <div className="social-icons">
+                        {/* <a href="https://www.facebook.com/absheroncareer/" target="_blank">
                             <img src={fbIMG} class=" No-Select" />
-                            </a>
-                            <a href="https://www.instagram.com/absheroncareer/" target="_blank">
+                        </a> */}
+                        <a href="https://www.linkedin.com/company/103794108/" target="_blank">
+                            <img src={inIMG} class=" No-Select" />
+                        </a>
+                        {/* <a href="https://www.instagram.com/absheroncareer/" target="_blank">
                             <img src={igIMG} class=" No-Select" />
-                            </a>
-                            <a href="" target="_blank">
+                        </a> */}
+                        {/* <a href="" target="_blank">
                             <img src={xIMG} class=" No-Select" />
-                            </a>
+                        </a> */}
 
-                        </div> */}
-                    </a>
+                    </div>
                 </div>
             </div>
         </div>
